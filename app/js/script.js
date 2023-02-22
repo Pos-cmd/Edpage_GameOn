a1ad5cd21f0ad6bcfb4996d4a53a25a604bcc07f
Pos-cmd <afolabiyasser06@gmail.com>
2023-02-22 18:14:32 +0000

    Finission de la mise en place du main et footer

--- app/js/script.js
+++ app/js/script.js
@@ -6,10 +6,12 @@ const menu__togle = document.querySelector(".header__menu--toggle");
 menu.addEventListener("click", function () {
   header.classList.toggle("open");
   if (header.classList.contains("open")) {
+    overlay.style.display = "block";
     overlay.style.opacity = 1;
     menu__togle.style.display = "flex";
   } else {
     menu__togle.style.display = "none";
     overlay.style.opacity = 0;
+    overlay.style.display = "none";
   }
 });
